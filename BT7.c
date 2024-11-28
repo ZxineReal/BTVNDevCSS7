365b2c3b7191b737397c32d02e86cbec1c293576
ZxineReal <stargolem90@gmail.com>
2024-11-28 09:30:34 +0700

    Add files via upload

---
+++ BT7.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main(){
+	int num;
+	int numb;
+	int count = 0;
+	printf("Moi ban nhap vao so luong phan tu cua mang: \n");
+	scanf("%d",&num);
+	int a[num];
+	
+	for(int i=0; i<num; i++){
+		while(1){
+		
+		printf("Moi ban nhap vao array[%d] :\n",i+1);
+		scanf("%d",&numb);
+		
+		if(numb%2!=0){
+			a[i]=numb;
+			break;
+		}else{
+			printf("Khong phai so le, xin moi nhap lai\n");
+		}
+		
+	}
+	}
+	for(int i=0;i<num;i++){
+		printf("array[%d] : %d\n",i+1,a[i]);
+	}
+
+	
+}
